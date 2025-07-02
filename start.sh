9d7addb053aaead27a4f711aca68acdae5c0da1e
GrammarAzi <githubnewprofile1@gmail.com>
2025-07-02 23:23:28 +0400

    change

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ echo "🛠 Applying all migrations..."
 python manage.py migrate --noinput
 
 echo "🚀 Starting Celery worker in background..."
-celery -A auth_service worker --loglevel=info &
+celery -A grammar worker --loglevel=info &
 
 echo "🌐 Starting Django server..."
 python manage.py runserver 0.0.0.0:8000
