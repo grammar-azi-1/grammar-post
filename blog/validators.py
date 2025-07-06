4ae35436751ce52d4960ee81220d927c49de3cc9
Mahammad Farajov14 <mahammadfarajov1029876543@gmail.com>
2025-07-06 15:14:42 +0400

    changes

--- blog/validators.py
+++ blog/validators.py
@@ -5,6 +5,9 @@ import os
 def validate_file_type(value):
     typefile = os.path.splitext(value.name)[1].lower()
     print(typefile)
-    allowed_extensions = ['.png', '.jpg']
+    allowed_extensions =[".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".tif",
+    ".webp", ".heic", ".heif", ".avif", ".svg", ".ico", ".raw",
+    ".psd", ".ai", ".eps", ".indd"]
+
     if typefile not in allowed_extensions:
-        raise ValidationError('Wrong file type! Allowed file types are: PDF, DOCX, TXT, PNG, JPG')
+        raise ValidationError('Wrong file type! Must use any of these formats : ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".tif",".webp", ".heic", ".heif", ".avif", ".svg", ".ico", ".raw",".psd", ".ai", ".eps", ".indd"')
