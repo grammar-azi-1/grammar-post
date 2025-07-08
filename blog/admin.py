82ce371de741e0d9bf6c2b1be65ec2e4957d7720
Mahammad Farajov14 <mahammadfarajov1029876543@gmail.com>
2025-07-08 12:02:54 +0400

    changes

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from blog.models import Post, Comment
+from blog.models import Post, Comment, Notification
 
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.site.register(Notification)
