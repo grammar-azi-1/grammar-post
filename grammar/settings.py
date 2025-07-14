6f17b19f8ae6f7b8c2f9b736ab2a4e17ff95f9ee
GrammarAzi <githubnewprofile1@gmail.com>
2025-07-14 15:15:31 +0400

    change

--- grammar/settings.py
+++ grammar/settings.py
@@ -301,7 +301,7 @@ SWAGGER_SETTINGS = {
 }
 
 # AWS S3 configurations
- = os.getenv('')
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'eu-north-1')
