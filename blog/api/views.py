471b8d498464e2a8067803346256f35210518a13
GrammarAzi <githubnewprofile1@gmail.com>
2025-07-17 01:03:27 +0400

    change

--- blog/api/views.py
+++ blog/api/views.py
@@ -213,7 +213,7 @@ class PostLikeAPIView(APIView):
                         "type": "send_notification",
                         "data": {
                             "notification_id": notification.id,
-                            "type": "like",
+                            "type": "like_post",
                             "sender": user.username,
                             "post_id": post.id,
                             "message": f"{user.username} sizin postunuzu bəyəndi"
